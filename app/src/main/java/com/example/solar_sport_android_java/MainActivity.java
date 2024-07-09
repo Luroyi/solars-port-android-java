4467397ee73ff90278c35b94fa13fc04432a367e
Luroyi <yinethmendoza1997@gmail.com>
2024-07-09 00:05:36 -0500

    Firts commit Crear vista de aterrizaje Pantalla inicio de sesion

--- app/src/main/java/com/example/solar_sport_android_java/MainActivity.java
+++ app/src/main/java/com/example/solar_sport_android_java/MainActivity.java
@@ -11,6 +11,9 @@ import androidx.core.view.WindowInsetsCompat;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.os.Handler;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -24,12 +27,28 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        Button loginButton = findViewById(R.id.login_button);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        LinearLayout linearLayout1 = findViewById(R.id.linearLayout1);
+        LinearLayout linearLayout2 = findViewById(R.id.linearLayout2);
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                linearLayout1.setVisibility(View.VISIBLE);
+                linearLayout2.setVisibility(View.GONE);
+            }
+        },3000);
+
+
+        Button loginGoButton = findViewById(R.id.iniciar_sesion_button);
+        loginGoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
+                System.out.print("entro");
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
+
     }
 }

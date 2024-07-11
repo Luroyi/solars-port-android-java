8f6cb33ef7ccf4ac03c6790de5cf83cd1aa49e4a
Luroyi <yinethmendoza1997@gmail.com>
2024-07-11 12:36:22 -0500

    Renombar actividades

--- app/src/main/java/com/example/solar_sport_android_java/MainScreenActivity.java
+++ app/src/main/java/com/example/solar_sport_android_java/MainScreenActivity.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class RecuperarContrasenaActivity extends AppCompatActivity {
+public class MainScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.recuperar_contrasena);
+        setContentView(R.layout.main_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
